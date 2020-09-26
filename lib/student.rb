4eb4add6de9f72c009a0eeb0e2c21c3f3e50db28
Rocio Batres <jonasgirl21@gmail.com>
2020-09-26 18:26:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative ./user.rb
 
 class Student < User 
 
+def initialize
+  @knowledge = []
+end 
+
+def learn(info) 
+  @knowledge << info
+end 
+
+def knowledge
+  @knowledge
+end 
+
 end
